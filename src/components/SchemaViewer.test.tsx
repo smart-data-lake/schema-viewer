4f34519a117c1648551a778ca0b0f2513a131735
ddb <david.deuber@elca.ch>
2023-06-08 12:44:48 +0200

    Mock svg property to prevent test failure

--- src/components/SchemaViewer.test.tsx
+++ src/components/SchemaViewer.test.tsx
@@ -44,7 +44,29 @@ function defineMissingProperties(): void {
     }),
   });
 
-  Object.defineProperty(URL, 'createObjectURL', {
+  Object.defineProperty(global.SVGElement.prototype, 'viewBox', {
+    writable: true,
+    value: {
+      animVal:  {
+        x: 0,
+        y: 0,
+        left: 0,
+        right: 0,
+        top: 0,
+        bottom: 0,
+        width: 0,
+        height: 0
+      },
+      baseVal: {
+        x: 0,
+        y: 0,
+        height: 0,
+        width: 0
+      }
+    }
+  });
+
+  Object.defineProperty(global.URL, 'createObjectURL', {
     writable: true,
     value: jest.fn()
   });
